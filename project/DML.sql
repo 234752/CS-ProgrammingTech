c12ada0640b289a9182c967bc0052286f8a25ced
Patryk <patryk.gielcz@gmail.com>
2022-05-06 22:08:13 +0200

    inserts

---
+++ project/DML.sql
@@ -0,0 +1,170 @@
+INSERT INTO suppliers VALUES (1, 'Rockwell Diamonds', 'Canada');
+INSERT INTO suppliers VALUES (2, 'Anglo American plc', 'United Kingdom');
+INSERT INTO suppliers VALUES (3, 'Gem Diamonds', 'United Kingdom');
+INSERT INTO suppliers VALUES (4, 'Petra Diamonds', 'United States');
+INSERT INTO suppliers VALUES (5, 'Lucara', 'Canada');
+INSERT INTO suppliers VALUES (6, 'Dominion', 'Canada');
+INSERT INTO suppliers VALUES (7, 'Debswana', 'Botswana');
+INSERT INTO suppliers VALUES (8, 'Rio-Tinto', 'Australia');
+INSERT INTO suppliers VALUES (9, 'Alrosa', 'Russia');
+INSERT INTO suppliers VALUES (10, 'De Beers', 'United Kingdom');
+
+
+INSERT INTO products VALUES (1, 1, 'Diamond', 50000, 'White');
+INSERT INTO products VALUES (2, 2, 'Emerald', 10000, 'Green');
+INSERT INTO products VALUES (3, 3, 'Opal', 8000, 'Blue');
+INSERT INTO products VALUES (4, 4, 'Ruby', 20000, 'Red');
+INSERT INTO products VALUES (5, 5, 'Sapphire', 9000, 'Blue');
+INSERT INTO products VALUES (6, 6, 'Spinel', 6000, 'Red');
+INSERT INTO products VALUES (7, 7, 'Topaz', 12000, 'White');
+INSERT INTO products VALUES (8, 8, 'Amber', 7000, 'Orange');
+INSERT INTO products VALUES (9, 9, 'Amethyst', 18000, 'Purple');
+INSERT INTO products VALUES (10, 10, 'Beryl', 14000, 'Blue');
+INSERT INTO products VALUES (11, 1, 'Calcite', 1000, 'White');
+INSERT INTO products VALUES (12, 2, 'Citrine', 16000, 'Yellow');
+INSERT INTO products VALUES (13, 3, 'Fluorite', 8000, 'White');
+INSERT INTO products VALUES (14, 4, 'Garnet', 11000, 'Red');
+INSERT INTO products VALUES (15, 5, 'Jadeite', 13000, 'Green');
+INSERT INTO products VALUES (16, 6, 'Lapis Lazuli', 15000, 'Blue');
+INSERT INTO products VALUES (17, 1, 'Malachite', 20000, 'Green');
+INSERT INTO products VALUES (18, 2, 'Obsidian', 15000, 'Black');
+INSERT INTO products VALUES (19, 3, 'Onyx', 10000, 'Black');
+INSERT INTO products VALUES (20, 1, 'Quartz', 6000, 'White');
+
+
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (1, 'Des', 'Kilbane', 'Male', 'dkilbane0@ftc.gov', '860-269-219', 8421, 28);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (2, 'Valdemar', 'Simonson', 'Male', 'vsimonson1@unc.edu', '438-514-925', 6170, 13);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (3, 'Augustina', 'Fancott', 'Female', 'afancott2@nymag.com', '124-401-583', 11219, 5);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (4, 'Cordelie', 'Hinchon', 'Female', 'chinchon3@hubpages.com', '229-358-677', 9175, 23);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (5, 'Barbra', 'Greenley', 'Female', 'bgreenley4@shinystat.com', '242-610-503', 9690, 5);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (6, 'Cybil', 'Fouch', 'Female', 'cfouch5@parallels.com', '902-779-839', 10205, 24);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (7, 'Cassandra', 'Grass', 'Female', 'cgrass6@sakura.ne.jp', '170-278-573', 7238, 8);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (8, 'Billie', 'Khomishin', 'Other', 'bkhomishin7@comcast.net', '436-604-383', 6299, 16);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (9, 'Miriam', 'Macewan', 'Other', 'mmacewan8@gov.uk', '708-852-376', 9993, 27);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (10, 'Daphna', 'Pendre', 'Female', 'dpendre9@tuttocitta.it', '800-930-162', 9482, 11);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (11, 'Bradford', 'Hatchett', 'Male', 'bhatchetta@dagondesign.com', '601-100-130', 9274, 19);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (12, 'Justin', 'Vasyagin', 'Male', 'jvasyaginb@slate.com', '404-442-478', 10071, 25);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (13, 'Gaile', 'Swaisland', 'Male', 'gswaislandc@virginia.edu', '798-141-005', 12373, 11);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (14, 'Catherina', 'Rawson', 'Female', 'crawsond@reuters.com', '144-416-396', 8987, 21);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (15, 'Regen', 'Mainwaring', 'Male', 'rmainwaringe@quantcast.com', '611-273-772', 7485, 9);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (16, 'Shepperd', 'Ferie', 'Male', 'sferief@soundcloud.com', '515-375-418', 6502, 6);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (17, 'Florrie', 'Lilleman', 'Female', 'flillemang@intel.com', '665-737-177', 8836, 13);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (18, 'Farlay', 'Crowcum', 'Male', 'fcrowcumh@google.it', '483-904-765', 7785, 20);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (19, 'Dore', 'Bowery', 'Other', 'dboweryi@4shared.com', '947-920-769', 9167, 17);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (20, 'Jeddy', 'Thurgood', 'Male', 'jthurgoodj@theatlantic.com', '153-319-108', 6337, 26);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (21, 'Inger', 'Baddam', 'Male', 'ibaddamk@blogger.com', '495-952-317', 8297, 9);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (22, 'Lavinia', 'Bowkley', 'Female', 'lbowkleyl@shareasale.com', '906-784-008', 3861, 20);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (23, 'Xenia', 'Ainscow', 'Female', 'xainscowm@nature.com', '315-295-690', 5260, 11);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (24, 'Yehudi', 'Mawer', 'Male', 'ymawern@techcrunch.com', '235-278-756', 10954, 15);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (25, 'Durward', 'Broadwood', 'Male', 'dbroadwoodo@squidoo.com', '898-236-859', 8771, 27);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (26, 'Sharleen', 'Waiton', 'Female', 'swaitonp@state.tx.us', '847-685-458', 5892, 9);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (27, 'Holly', 'Guisby', 'Male', 'hguisbyq@wired.com', '319-990-313', 10984, 19);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (28, 'Waverley', 'Schach', 'Male', 'wschachr@mtv.com', '429-694-749', 11138, 15);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (29, 'Jacquenetta', 'Burhill', 'Female', 'jburhills@shop-pro.jp', '295-432-239', 7991, 15);
+insert into EMPLOYEES (employee_id, first_name, last_name, gender, employee_email, employee_phone, salary, provision) 
+	VALUES (30, 'Lorelle', 'Bloodworth', 'Female', 'lbloodwt@theglobail.com', '289-763-860', 5604, 27);
+
+
+
+INSERT INTO CUSTOMERS  VALUES (1, 'Mathew', 'Jahnig', 'Male', 'mjahnig0@pagso.fr', '532-269-393', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (2, 'Bengt', 'Tennock', 'Male', 'btennock1@gtar.com', '733-935-308', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (3, 'Sheff', 'Pollington', 'Male', 'spollington2@irs.gov', '278-418-763', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (4, 'Dollie', 'Andretti', 'Other', 'dai3@reuters.com', '535-951-422', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (5, 'Arman', 'Lyons', 'Male', 'alyons4@re.com', '882-728-897', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (6, 'Allene', 'Abbet', 'Female', 'aabbet5@liage.com', '414-593-313', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (7, 'Edythe', 'O''Heneghan', 'Female', 'eohn6@gmpg.org', '193-872-736', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (8, 'Murdock', 'Hunnaball', 'Male', 'mhll7@t.co', '124-611-892', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (9, 'Mickie', 'Van Schafflaer', 'Male', 'mvans8@webs.com', '376-260-843', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (10, 'Bo', 'Slayton', 'Male', 'bslan9@ted.com', '916-597-919', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (11, 'Sumner', 'Ruvel', 'Male', 'sruvela@sbwire.com', '246-779-772', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (12, 'Quint', 'Barnson', 'Male', 'qbarb@harvard.edu', '691-354-789', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (13, 'Devin', 'MacAnulty', 'Male', 'dmactyc@ibm.com', '551-171-680', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (14, 'Rosene', 'Balfour', 'Female', 'rbalrd@parse.fr', '375-955-758', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (15, 'Duffie', 'Isselee', 'Male', 'disseleee@about.com', '590-715-352', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (16, 'Malinde', 'Rushbury', 'Female', 'mruryf@google.co.uk', '157-735-815', 0);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (17, 'Kareem', 'Brass', 'Male', 'kbra@dion.ne.jp', '615-174-320', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (18, 'Bessy', 'Garner', 'Female', 'bgah@goo.gl', '914-591-273', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (19, 'Adelbert', 'Tawn', 'Male', 'atawni@over-blog.com', '228-889-611', 1);
+INSERT INTO customers (customer_id, first_name, last_name, gender, customer_email, customer_phone, premium) VALUES (20, 'Joan', 'Carreck', 'Female', 'jcar@vine.com', '547-594-285', 0);
+
+
+
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (1,6,18,30,'28-Jan-2022',9); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (2,15,2,17,'30-Mar-2022',10); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (3,4,3,18,'24-Mar-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (4,20,2,29,'11-Oct-2021',3); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (5,17,5,5,'22-Apr-2022',3); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (6,17,3,17,'19-Dec-2021',5); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (7,2,7,8,'18-Mar-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (8,10,17,23,'10-Nov-2021',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (9,12,12,10,'16-Jan-2022',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (10,19,12,1,'04-Dec-2021',4); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (11,15,13,18,'18-Dec-2021',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (12,15,7,7,'14-Dec-2021',8); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (13,18,7,15,'04-May-2022',9); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (14,1,4,7,'02-Jan-2022',3); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (15,12,19,17,'28-Feb-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (16,16,2,22,'07-Oct-2021',8); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (17,17,5,1,'10-Dec-2021',10); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (18,8,16,30,'16-Nov-2021',8); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (19,6,16,7,'16-Feb-2022',8); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (20,18,1,17,'28-Dec-2021',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (21,11,2,24,'06-Apr-2022',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (22,14,3,30,'10-Apr-2022',5); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (23,10,19,24,'08-Nov-2021',10); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (24,16,15,16,'19-Oct-2021',6); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (25,20,8,30,'14-Apr-2022',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (26,1,7,26,'29-Oct-2021',4); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (27,13,1,6,'25-Feb-2022',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (28,13,9,20,'13-Jan-2022',9); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (29,16,4,29,'08-Dec-2021',9); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (30,5,3,13,'17-Mar-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (31,10,7,25,'11-Jan-2022',5); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (32,9,4,6,'18-Feb-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (33,11,15,21,'05-Jan-2022',3); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (34,2,13,5,'18-Apr-2022',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (35,12,5,9,'13-Nov-2021',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (36,7,9,12,'03-Dec-2021',9); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (37,5,8,9,'19-Jan-2022',10); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (38,14,6,23,'09-Jan-2022',4); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (39,4,4,1,'27-Apr-2022',3); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (40,19,11,11,'16-Oct-2021',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (41,4,11,18,'17-Jan-2022',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (42,5,10,20,'29-Jan-2022',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (43,16,16,14,'06-Nov-2021',2); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (44,2,20,23,'09-Nov-2021',8); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (45,15,18,26,'17-Dec-2021',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (46,7,12,15,'26-Nov-2021',1); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (47,16,12,30,'30-Mar-2022',4); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (48,13,1,3,'26-Feb-2022',6); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (49,2,10,17,'22-Dec-2021',7); 
+INSERT INTO ORDERS(ORDER_ID,PRODUCT_ID,CUSTOMER_ID,EMPLOYEE_ID,ORDER_DATE,PRODUCT_WEIGHT) VALUES (50,11,11,1,'06-Apr-2022',9);
